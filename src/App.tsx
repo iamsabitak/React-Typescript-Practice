76551b745e8f16ed478dc362915f1664b127fb67
sabita <sabitakhadka091@gmail.com>
2024-03-27 15:59:02 +0545

    implement the gallery component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NameInput from "./components/NameInput";
+import Gallery from "./components/Gallery";
 
 const App: React.FC = () => {
   return (
     <div>
       <NameInput initialFirstName="John" initialLastName="Doe" />
+      <Gallery />
     </div>
   );
 };
