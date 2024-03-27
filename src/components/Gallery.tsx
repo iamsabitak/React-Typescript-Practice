ca523724e6086b212b5a056e9d9ab16f600793f1
sabita <sabitakhadka091@gmail.com>
2024-03-27 15:52:59 +0545

    created  an array of imgae

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 const Gallery: React.FC = () => {
+  const imageUrls = [
+    "https://img.freepik.com/premium-vector/sunset-with-lots-buildings-along-road_1035899-2289.jpg",
+    "https://i.pinimg.com/736x/33/f1/59/33f159819ccff1704818e427e3613500.jpg",
+    "https://img.freepik.com/premium-photo/90s-japanese-animation-kunihiko-ikuhara-style-city-vie-generative-ai_791316-14475.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9g-cyN70-qR1tGnYFy9AambDYOmu0mActj2nr9FdUmGV2_FykC798gkduK1_UYrTM_RQ&usqp=CAU",
+  ];
   return <div>Gallery</div>;
 };
 
