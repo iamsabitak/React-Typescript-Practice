bfd9a9c801283b68e76bbecf575afb7d283fc3a2
sabita <sabitakhadka091@gmail.com>
2024-03-27 14:54:49 +0545

    Updated NameInput component to display static full name

--- src/components/NameInput.tsx
+++ src/components/NameInput.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const NameInput: React.FC = () => {
-  return <div>NameInput</div>;
+  return (
+    <div>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input id="firstName" type="text" />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input id="lastName" type="text" />
+      </div>
+      <p>
+        Full Name: <strong>Sabita</strong>
+      </p>
+    </div>
+  );
 };
 
 export default NameInput;
