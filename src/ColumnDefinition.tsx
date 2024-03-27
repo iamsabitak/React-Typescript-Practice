de28168b1fb58ecb181281e97f7b3bfdbf13889c
sabita <sabitakhadka091@gmail.com>
2024-03-27 14:48:12 +0545

    created a new component NameINput

--- src/ColumnDefinition.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ColumnDefinition: React.FC = () => {
-  return <div>ColumnDefinition</div>;
-};
-
-export default ColumnDefinition;
---
+++ src/components/NameInput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NameInput: React.FC = () => {
+  return <div>NameInput</div>;
+};
+
+export default NameInput;
