b3bba4425475938f9d23dc78ff6c68d80632ecc9
sabita <sabitakhadka091@gmail.com>
2024-04-01 10:50:30 +0545

    added textinput

--- src/components/PostApi.tsx
+++ src/components/PostApi.tsx
@@ -1,7 +1,27 @@
+import { TextInput } from "@mantine/core";
 import React from "react";
 
 const PostApi: React.FC = () => {
-  return <div>PostApi</div>;
+  
+  return (
+    <div>
+      <>
+        <TextInput
+          leftSectionPointerEvents="none"
+          // leftSection={icon}
+          label="Your email"
+          placeholder="Your email"
+        />
+        <TextInput
+          mt="md"
+          rightSectionPointerEvents="none"
+          // rightSection={icon}
+          label="Your email"
+          placeholder="Your email"
+        />
+      </>
+    </div>
+  );
 };
 
 export default PostApi;
