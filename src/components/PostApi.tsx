674b9d00a3d0b7e56e1348cb51f0a413ec893126
sabita <sabitakhadka091@gmail.com>
2024-04-01 11:29:17 +0545

    mplemented a form component Mantine and Axios for making POST requests to an API endpoint

--- src/components/PostApi.tsx
+++ src/components/PostApi.tsx
@@ -1,25 +1,44 @@
-import { TextInput } from "@mantine/core";
-import React from "react";
+import { Button, TextInput } from "@mantine/core";
+import axios from "axios";
+import React, { useState } from "react";
 
 const PostApi: React.FC = () => {
-  
+  const [email, setEmail] = useState<string>();
+  const [name, setName] = useState<string>();
+
+  const postData = async () => {
+    const datas = { email: email, name: name };
+    const res = await axios.post("https://jsonplaceholder.typicode.com/posts", {
+      datas,
+    });
+    console.log(res);
+  };
+
+  const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const nameChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
   return (
     <div>
-      <>
-        <TextInput
-          leftSectionPointerEvents="none"
-          // leftSection={icon}
-          label="Your email"
-          placeholder="Your email"
-        />
-        <TextInput
-          mt="md"
-          rightSectionPointerEvents="none"
-          // rightSection={icon}
-          label="Your email"
-          placeholder="Your email"
-        />
-      </>
+      <TextInput
+        label="Full Name"
+        placeholder="Full Name"
+        name="name"
+        value={name}
+        onChange={nameChangeHandler}
+      />
+      <TextInput
+        mt="md"
+        label="Your email"
+        name="email"
+        placeholder="Your email"
+        value={email}
+        onChange={emailChangeHandler}
+      />
+      <Button onClick={postData}>Submit</Button>
     </div>
   );
 };
