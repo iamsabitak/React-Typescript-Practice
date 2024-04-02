044f3aadaf4ea943e0c16627fe3c00c7c953132b
sabita <sabitakhadka091@gmail.com>
2024-04-02 14:02:37 +0545

    Add data fetching functionality in Promises component

--- src/components/Promises.tsx
+++ src/components/Promises.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 
 const Promises: React.FC = () => {
+  const fetchData = () => {
+    console.log(true);
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        console.log(false);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        console.log(false);
+      });
+  };
+  fetchData();
   return <div>Promises</div>;
 };
 
