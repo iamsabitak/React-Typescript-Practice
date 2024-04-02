e223c6655087ca1b5144dd103bd4cb4f49f71c95
sabita <sabitakhadka091@gmail.com>
2024-04-02 16:11:25 +0545

    Implement FetchData function within DataTwo to asynchronously handle PromiseTwo

--- src/components/promisee.tsx
+++ src/components/promisee.tsx
@@ -18,13 +18,33 @@ function promisee() {
     });
   };
 
-  const FetchData = async () => {};
-
+  function DataTwo() {
+    const PromiseTwo = new Promise((resolve, reject) => {
+      console.log("promise two");
+      const error = true;
+      setTimeout(() => {
+        if (!error) {
+          resolve({ userName: "SabitaKhadka", password: "1234" });
+        } else {
+          reject("Something went wrong");
+        }
+      }, 1000);
+    });
+    const FetchData = async () => {
+      try {
+        const res = await PromiseTwo;
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    FetchData();
+  }
   return (
     <div>
       promisee
       <Button onClick={UserData}>PromiseOne</Button>
-      <Button onClick={FetchData}>FetchData</Button>
+      <Button onClick={DataTwo}>PromiseTwo</Button>
     </div>
   );
 }
