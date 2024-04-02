bfed2be3a35e7308bc20e3063edb195582a9d00a
sabita <sabitakhadka091@gmail.com>
2024-04-02 16:04:13 +0545

    Implement asynchronous promise for fetching user data

--- src/components/promisee.tsx
+++ src/components/promisee.tsx
@@ -1,5 +1,32 @@
+import { Button } from "@mantine/core";
+
 function promisee() {
-  return <div>promisee</div>;
+  const UserData = () => {
+    const PromiseOne = new Promise((resolve, reject) => {
+      console.log("promise one");
+      const error = false;
+      setTimeout(() => {
+        if (!error) {
+          resolve({ userName: "SabitaKhadka", password: "1234" });
+        } else {
+          reject("Something went wrong");
+        }
+      }, 1000);
+    });
+    PromiseOne.then((data) => console.log(data, "resolved ")).catch((error) => {
+      console.log(error, "Error");
+    });
+  };
+
+  const FetchData = async () => {};
+
+  return (
+    <div>
+      promisee
+      <Button onClick={UserData}>PromiseOne</Button>
+      <Button onClick={FetchData}>FetchData</Button>
+    </div>
+  );
 }
 
 export default promisee;
