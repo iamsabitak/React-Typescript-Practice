4dc73aca149d51446aefad65adfa7adffb3c731d
sabita <sabitakhadka091@gmail.com>
2024-04-03 14:40:06 +0545

    added counter which add and remove the va;ue

--- src/components/memoization/Parent.tsx
+++ src/components/memoization/Parent.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Child from "./Child";
 
 const Parent: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    console.log("object1");
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+    console.log("object2");
+  };
+
   return (
     <div>
       Parent
+      <div>
+        <h2>Counter</h2>
+        <div>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+        </div>
+        <p>Count: {count}</p>
+      </div>
       <Child />
     </div>
   );
