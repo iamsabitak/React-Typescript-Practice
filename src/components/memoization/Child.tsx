4c3718dc5621c0623b18f59bdc054c6e7ec17356
sabita <sabitakhadka091@gmail.com>
2024-04-03 13:58:34 +0545

    created a new components parent and child

---
+++ src/components/memoization/Child.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Child: React.FC = () => {
+  return <div>Child</div>;
+};
+
+export default Child;
---
+++ src/components/memoization/Parent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Parent: React.FC = () => {
+  return <div>Parent</div>;
+};
+
+export default Parent;
