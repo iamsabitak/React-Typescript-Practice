863c4fa82f00cb3450d19e2beb0acf2ba761f770
sabita <sabitakhadka091@gmail.com>
2024-04-10 14:27:43 +0545

    Implement  and  functions

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 
 const AddTodo: React.FC = () => {
-    const [AddTodo, setAddTodo] = useState<string>("");
+  const [AddTodo, setAddTodo] = useState<string>("");
   const [todos, setTodos] = useState<string[]>([]);
+
+  const onAdd = () => {
+    setTodos([...todos, AddTodo]);
+  };
+
+  const deleteTodo = (index: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.filter((_, itemIndex) => itemIndex !== index)
+    );
+  };
   return <div>AddTodo</div>;
 };
 
