c7ddf03bc40b1478f238fb4222c12bfd9fbc69b1
sabita <sabitakhadka091@gmail.com>
2024-04-10 14:26:26 +0545

    Initialize state variables

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTodo: React.FC = () => {
+    const [AddTodo, setAddTodo] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
   return <div>AddTodo</div>;
 };
 
